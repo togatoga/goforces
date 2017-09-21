56789a89740f1a790376612a470ec20ddad2693f
togatoga <togasakitogatoga@gmail.com>
2017-09-21 23:47:07 +0900

    Add problem.go

---
+++ problem.go
@@ -0,0 +1,10 @@
+package goforces
+
+type Problem struct {
+	ContestID int64 `json:"contestId"`
+	Index     string `json:"index"`
+	Name      string `json:"name"`
+	Type      string `json:"type"`
+	Points    float32 `json:"points"`
+	Tags      []string `json:"tags"`
+}
