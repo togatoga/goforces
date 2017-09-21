2b4e9884d18ec4ce0307ee2d32c950414eb358b3
togatoga <togasakitogatoga@gmail.com>
2017-09-21 23:47:33 +0900

    Add codeforces.go

--- codeforces.go
+++ codeforces.go
@@ -1,1 +1,26 @@
 package goforces
+
+import (
+	"net/url"
+	"net/http"
+	"log"
+	"io/ioutil"
+)
+
+type Client struct {
+	URL        *url.URL
+	HTTPClient *http.Client
+	Logger     *log.Logger
+}
+
+func NewClient(urlStr string, logger *log.Logger) (*Client, error) {
+	parsedURL, err := url.ParseRequestURI(urlStr)
+	if err != nil {
+		return nil, err
+	}
+	var discardLogger = log.New(ioutil.Discard, "", log.LstdFlags)
+	if logger == nil {
+		logger = discardLogger
+	}
+	return &Client{URL: parsedURL, Logger: logger}, nil
+}
