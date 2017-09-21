01a9512ea1db1a0e2fd4306c50999c1b8974e4d9
togatoga <togasakitogatoga@gmail.com>
2017-09-21 23:47:51 +0900

    Add problemset.go

--- problemset.go
+++ problemset.go
@@ -1,1 +1,9 @@
 package goforces
+
+type ProblemsResponse struct {
+	Status string `json:"status"`
+	Result struct {
+		Problems          []Problem `json:"problems"`
+		ProblemStatistics ProblemStatistics `json:"problemStatistics"`
+	} `json:"result"`
+}
