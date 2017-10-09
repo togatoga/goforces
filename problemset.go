384e42feada61468f3964c0befd54028de1f091f
togatoga <togasakitogatoga@gmail.com>
2017-10-09 14:23:01 +0900

    Fix seet resp.Result

--- problemset.go
+++ problemset.go
@@ -13,15 +13,7 @@ type Problems struct {
 }
 
 func (c *Client) GetProblems(ctx context.Context, tags []string) (*Problems, error) {
-
 	c.Logger.Println("GetProblems tags: ", tags)
-	type ProblemsResponse struct {
-		Status string   `json:"status"`
-		Result Problems `json:"result"`
-	}
-
-	var resp ProblemsResponse
-
 	v := url.Values{}
 	v.Add("tags", strings.Join(tags, ";"))
 	spath := "/problemset.problems" + "?" + v.Encode()
@@ -33,6 +25,11 @@ func (c *Client) GetProblems(ctx context.Context, tags []string) (*Problems, err
 	if err != nil {
 		return nil, err
 	}
+	type ProblemsResponse struct {
+		Status string   `json:"status"`
+		Result Problems `json:"result"`
+	}
+	var resp ProblemsResponse
 	if err := decodeBody(res, &resp); err != nil {
 		return nil, err
 	}
@@ -41,5 +38,6 @@ func (c *Client) GetProblems(ctx context.Context, tags []string) (*Problems, err
 		return nil, fmt.Errorf("Status Error: %s", res.Status)
 	}
 	var problems Problems
-	return &problemResponse, nil
+	problems = resp.Result
+	return &problems, nil
 }
