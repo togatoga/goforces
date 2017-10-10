b2599c233e91df88028c6a72906ede9297b2e1ae
togatoga <togasakitogatoga@gmail.com>
2017-10-10 19:47:41 +0900

    Add party.go

---
+++ party.go
@@ -0,0 +1,9 @@
+package goforces
+
+type Party struct {
+	ContestID        int    `json:"contestId"`
+	Members          Member `json:"members"`
+	ParticipantType  string `json:"participantType"`
+	Ghost            bool   `json:"ghost"`
+	StartTimeSeconds int    `json:"startTimeSeconds"`
+}
