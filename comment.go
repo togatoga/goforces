a4782e3bb74177cb88c9748c39ea7976a70a6955
togatoga <togasakitogatoga@gmail.com>
2017-10-11 06:39:33 +0900

    Add comment.go

---
+++ comment.go
@@ -0,0 +1,11 @@
+package goforces
+
+type Comment struct {
+	ID                  int    `json:"id"`
+	CreationTimeSeconds int    `json:"creationTimeSeconds"`
+	CommentatorHandle   string `json:"commentatorHandle"`
+	Locale              string `json:"locale"`
+	Text                string `json:"text"`
+	Rating              int    `json:"rating"`
+	ParentCommentID     int    `json:"parentCommentId,omitempty"`
+}
