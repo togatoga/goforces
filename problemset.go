51d3c331df641b3473f37bc3eaa487c255f9de32
togatoga <togasakitogatoga@gmail.com>
2017-10-10 19:56:09 +0900

    Add GetRecentStatus to problemset.go

--- problemset.go
+++ problemset.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/url"
+	"strconv"
 	"strings"
 )
 
@@ -40,4 +41,38 @@ func (c *Client) GetProblems(ctx context.Context, tags []string) (*Problems, err
 	var problems Problems
 	problems = resp.Result
 	return &problems, nil
+}
+
+func (c *Client) GetRecentStatus(ctx context.Context, count int) ([]Submission, error) {
+	c.Logger.Println("GetRecentStatus count: ", count)
+	if count <= 0 || count > 1000 {
+		return nil, fmt.Errorf("count value must be between 1 and 1000: %d", count)
+	}
+
+	v := url.Values{}
+	v.Add("count", strconv.Itoa(count))
+	spath := "/problemset.recentStatus" + "?" + v.Encode()
+	req, err := c.newRequest(ctx, "GET", spath, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	type RecentStatusResponse struct {
+		Status string       `json:"status"`
+		Result []Submission `json:"result"`
+	}
+	var resp RecentStatusResponse
+	if err := decodeBody(res, &resp); err != nil {
+		return nil, err
+	}
+	//check status
+	if resp.Status != "OK" {
+		return nil, fmt.Errorf("Status Error: %s", res.Status)
+	}
+	var submissions []Submission
+	submissions = resp.Result
+	return submissions, nil
 }
