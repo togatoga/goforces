be66e9f8e134a645c733718080e1727dd19282db
togatoga <togasakitogatoga@gmail.com>
2017-10-14 08:48:23 +0900

    Add GetContestList to contest.go

--- contest.go
+++ contest.go
@@ -47,3 +47,36 @@ func (c *Client) GetContestHacks(ctx context.Context, contestId int) ([]Hack, er
 	hacks = resp.Result
 	return hacks, nil
 }
+
+func (c *Client) GetContestList(ctx context.Context, gym bool) ([]Contest, error) {
+	c.Logger.Println("GetContestList gym: ", gym)
+	v := url.Values{}
+	if gym {
+		v.Add("gym", "true")
+	}
+	spath := "/contest.list" + "?" + v.Encode()
+	req, err := c.newRequest(ctx, "GET", spath, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	type Response struct {
+		Status string    `json:"status"`
+		Result []Contest `json:"result"`
+	}
+	var resp Response
+	if err := decodeBody(res, &resp); err != nil {
+		return nil, err
+	}
+	//check status
+	if resp.Status != "OK" {
+		return nil, fmt.Errorf("Status Error: %s", res.Status)
+	}
+
+	var contests []Contest
+	contests = resp.Result
+	return contests, nil
+}
