9ee8ec2856fcd26aaeb37e8f9ca4273893884817
togatoga <togasakitogatoga@gmail.com>
2017-10-14 15:33:22 +0900

    Fix Points float64

--- ranklistrow.go
+++ ranklistrow.go
@@ -3,7 +3,7 @@ package goforces
 type RanklistRow struct {
 	Party                     Party           `json:"party"`
 	Rank                      int64           `json:"rank"`
-	Points                    int64           `json:"points"`
+	Points                    float64         `json:"points"`
 	Penalty                   int64           `json:"penalty"`
 	SuccessfulHackCount       int64           `json:"successfulHackCount"`
 	UnsuccessfulHackCount     int64           `json:"unsuccessfulHackCount"`
