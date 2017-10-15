fc0628986f7641d824013eb8ac6ed203dbd599c5
togatoga <togasakitogatoga@gmail.com>
2017-10-15 22:00:43 +0900

    Add GetRecentAction to recentaction.go

--- recentaction.go
+++ recentaction.go
@@ -1,7 +1,50 @@
 package goforces
 
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"strconv"
+)
+
 type RecentAction struct {
 	TimeSeconds int       `json:"timeSeconds"`
 	BlogEntry   BlogEntry `json:"blogEntry"`
 	Comment     Comment   `json:"comment,omitempty"`
+}
+
+func (c *Client) GetRecentActions(ctx context.Context, maxCount int) ([]RecentAction, error) {
+	c.Logger.Println("GetRecentActions :", maxCount)
+
+	v := url.Values{}
+	if maxCount < 0 || maxCount > 100 {
+		return nil, fmt.Errorf("maxCount must be between 0 and 100")
+	}
+	//check options
+	v.Add("maxCount", strconv.Itoa(maxCount))
+
+	spath := "/recentActions" + "?" + v.Encode()
+	req, err := c.newRequest(ctx, "GET", spath, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	type Response struct {
+		Status string         `json:"status"`
+		Result []RecentAction `json:"result"`
+	}
+	var resp Response
+	if err := decodeBody(res, &resp); err != nil {
+		return nil, err
+	}
+
+	//check status
+	if resp.Status != "OK" {
+		return nil, fmt.Errorf("Status Error : %s", resp.Status)
+	}
+
+	return resp.Result, nil
 }
