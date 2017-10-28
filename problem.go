ab24d57f736f1bd79ad4dcefe52d9eebbbb0f3d0
togatoga <togasakitogatoga@gmail.com>
2017-10-29 00:15:05 +0900

    Add ProblemURL to problem.go

--- problem.go
+++ problem.go
@@ -1,11 +1,18 @@
 package goforces
 
+import "fmt"
+
 //Problem represents Codeforces problem
 type Problem struct {
-	ContestID int    `json:"contestId"`
+	ContestID int      `json:"contestId"`
 	Index     string   `json:"index"`
 	Name      string   `json:"name"`
 	Type      string   `json:"type"`
 	Points    float32  `json:"points"`
 	Tags      []string `json:"tags"`
+}
+
+//ProblemURL returns problem's url
+func (p Problem) ProblemURL() string {
+	return fmt.Sprintf("http://codeforces.com/contest/%d/problem/%s", p.ContestID, p.Index)
 }
